f8014b12bdd0e5909725d17cccc8aa3da897b00e
Uğur Demirer <osmanugurdemirer@gmail.com>
2023-10-12 20:55:57 +0300

    admin routes are added.

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const port = 3000;
 const apiRouter = express.Router();
 app.use('/api', apiRouter);
 
+// Adding routes to the router
 const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 apiRouter.use('/users', userRoutes);
+apiRouter.use('/users', adminRoutes);
 
+// App 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
